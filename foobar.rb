d0d646e4211309eaf84a74ee76a5eabce88ab377
Han Chen <hc0402@berkeley.edu>
2015-09-18 23:26:32 -0700

    submit hw1

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,14 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(array_of_integer)
+  	a = array_of_integer.uniq.map! {|i| (i.to_i + 2) % 2 == 0 ? i.to_i + 2 : array_of_integer.delete(i.to_i)}
+  	sum = 0
+  	for ele in a
+  		if ele != nil and ele <= 10
+			sum = sum + ele
+  		end
+  	end
+  	sum
+  end
+
 end
